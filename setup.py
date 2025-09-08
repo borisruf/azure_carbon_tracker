a9b6aa9949b601c12f6e5c2eb570d7110ac55af3
Boris Ruf <boris.ruf@axa.com>
2025-09-08 14:10:53 +0200

    fix structure

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="azure-carbon-tracker",
     version="0.1.0",
-    description="Azure carbon tracking via Cost Management API",
+    description="Azure carbon tracking via Cost Management API.",
     author="Boris Ruf",
     packages=find_packages(),
     include_package_data=True,
